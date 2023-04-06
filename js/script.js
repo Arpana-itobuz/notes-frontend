d22a33055f0fbb5c19eb01f8d80b6389a65a9111
Arpana <arpana@itobuz.com>
2023-04-06 19:13:46 +0530

    new update

--- js/script.js
+++ js/script.js
@@ -7,6 +7,11 @@ const plusButton = document.querySelector(".plus-button");
 const content = document.querySelector(".content");
 const textArea = document.querySelector(".text-area");
 const checkButton = document.querySelector(".check-button");
+const popUp = document.querySelector(".pop-up");
+const popUpDelete = document.querySelector(".pop-up-delete");
+const rejectButton = document.querySelector(".reject-button");
+
+// const allNotes = [];
 
 searchButton.addEventListener("click", () => {
   inputBox.classList.toggle("d-none");
@@ -21,14 +26,26 @@ crossButton.addEventListener("click", () => {
 });
 
 plusButton.addEventListener("click", () => {
+  rejectButton.classList.remove("d-none");
+  plusButton.classList.add("d-none");
   textArea.classList.remove("d-none");
   checkButton.classList.remove("d-none");
   textArea.value = "";
 });
 
-checkButton.addEventListener("click", () => {
+rejectButton.addEventListener("click", () => {
+  rejectButton.classList.add("d-none");
+  plusButton.classList.remove("d-none");
   textArea.classList.add("d-none");
   checkButton.classList.add("d-none");
+});
+
+checkButton.addEventListener("click", () => {
+  popUp.classList.remove("d-none");
+  textArea.classList.add("d-none");
+  checkButton.classList.add("d-none");
+  plusButton.classList.remove("d-none")
+  rejectButton.classList.add("d-none")
   let notes = [];
   notes.push(textArea.value);
   sendData(notes);
@@ -55,44 +72,63 @@ async function receiveData() {
       console.log(err);
     });
 
-  console.log(data);
+
+  // console.log(data);
+
+  // const result = data.data.filter((element) => {
+  //   if (inputBox.value === element.name) {
+  //     return element;
+  //   }
+  // });
+
+  // console.log("search resilt", result);
+
+  // for (let i = 0; i < data.data.length; i++) {
+  //   findButton.addEventListener("click", () => {
+  //     // console.log(data.data[i].name);
+  //     if (inputBox.value === data.data[i].name) {
+  //       content.innerHTML = `<div class="w-100 bg-white">
+  //       <div class="notes-area bg-black text-white w-100 py-5 my-3 p-2">
+  //       ${data.data[i].name}
+  //       </div>
+  //       </div>`;
+  //     } else {
+  //       alert("No such note found!!");
+  //     }
+  //   });
+  // }
 
   for (let i = 0; i < data.data.length; i++) {
-    findButton.addEventListener("click", () => {
-      if (inputBox.value === data.data[i].name) {
-        content.innerHTML = `<div class="w-100 bg-white">
-        <div class="notes-area bg-black text-white w-100 py-5 my-3 p-2">
-        ${data.data[i].name}
-        </div>
-        </div>`;
-      } else {
-        alert("No such note found!!");
-      }
-    });
-  }
-
-  for (let i = 0; i < data.data.length; i++) {
-    content.innerHTML += `<i class="fa-sharp fa-solid fa-trash bg-white position-absolute end-0 me-4 mt-2 p-1 rounded-circle delete-button" id=${data.data[i]._id} ></i><div class="notes-area bg-black text-white w-100 py-5 my-3 p-2">
+    content.innerHTML += `<i class="fa-sharp fa-solid fa-trash bg-white position-absolute end-0 me-4 mt-5 p-2 rounded-circle delete-button" onclick="deleteData('${data.data[i]._id}')" id='${data.data[i]._id}' ></i><div class="notes-area bg-black text-white w-100 py-5 my-3 p-2">
     ${data.data[i].name}
     </div>`;
     console.log(data.data[i].name);
   }
 
   const deleteButton = document.querySelectorAll(".delete-button");
+
   deleteButton.forEach((e) =>
     e.addEventListener("click", () => {
-      console.log("this is deleted");
-      deleteData(data.data._id);
-      content.innerHTML = "";
-      
-      receiveData();
+      // deleteButton.style.backgroundColor = "red";
+      popUpDelete.classList.remove("d-none");
     })
   );
+
+  // updateButton.forEach((e) =>
+  //   e.addEventListener("click", () => {
+  //     updateButton.style.backgroundColor = "red";
+
+  //   })
+  // );
+  return data;
 }
 
-receiveData();
+let responseData = receiveData();
+// allNotes.push(receiveData());
 
 async function deleteData(myId) {
+  console.log(myId);
+  // let myId = Id.toString();
   const deleteData = await fetch(`http://127.0.0.1:5000/`, {
     method: "DELETE",
     body: JSON.stringify({ id: myId }),
@@ -101,9 +137,15 @@ async function deleteData(myId) {
     },
   })
     .then((data) => {
+      content.innerHTML = "";
+
+      receiveData();
       return data.json();
     })
     .catch((err) => {
       console.log(err);
     });
+  console.log(deleteData);
 }
+
+// content.addEventListener();
